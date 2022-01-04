16e500f89e5a82c3ce8c5e464705f799018dd9ba
chance's mac mini <lqymaster@gmail.com>
2022-01-05 00:00:02 +0800

    update

--- Function/Convert.py
+++ Function/Convert.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 
 
 # 此函数旨在将一个文件夹内的所有DICOM文件转换为JPG文件，并集中生成在某一制定文件夹内
+# 目标文件夹必须是空的，否则会格式化
 def convert_DICOM(img, low_window, high_window, save_path):
     # 将重要数据储存到lungwin中
     lungwin = np.array([low_window * 1., high_window * 1.])
@@ -25,17 +26,15 @@ if __name__ == '__main__':
 
     path = input('jpg_file')
 
-    # 将路径里的内容格式化
     if os.path.exists(path):
         shutil.rmtree(path)
     os.makedirs(path)
 
-    for i in tqdm(os.listdir(dicom_dir)):
+    dicom_list = os.listdir(dicom_dir)
+    if os.path.exists(dicom_dir + '/.DS_Store'):
+        dicom_list.remove('.DS_Store')
 
-        if os.path.exists(dicom_dir + '/.DS_Store'):
-            os.remove(dicom_dir + '/.DS_Store')
-
-
+    for i in tqdm(dicom_list):
         # 读取dicom文件
         dcm_image_path = os.path.join(dicom_dir, i)
         # 分离文件名与扩展名
@@ -45,12 +44,7 @@ if __name__ == '__main__':
         jpg_path = os.path.join(path, name + '.jpg')
         # 读取dicom文件的相关信息
 
-        if os.path.exists(dicom_dir + '/.DS_Store'):
-            os.remove(dicom_dir + '/.DS_Store')
-
         ds_array = SimpleITK.ReadImage(dcm_image_path)
-
-
 
         # 获取array
         img_array = SimpleITK.GetArrayFromImage(ds_array)
