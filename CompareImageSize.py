f2b55b4f5c407af58eb8f866924103a48995c0fd
chance's mac mini <lqymaster@gmail.com>
2022-01-05 17:02:59 +0800

    update

--- CompareImageSize.py
+++ CompareImageSize.py
@@ -1,18 +1,26 @@
 from PIL import Image
 import os
 
-#  按照指定像素裁剪图片
+#  对比图片的尺寸，并找到最合适的裁剪尺寸
+
 JPG_file = input('The path of the JPG files: ')  # 图像文件目录
 JPG_list = os.listdir(JPG_file)
 if os.path.exists(JPG_file + '/.DS_Store'):
     JPG_list.remove('.DS_Store')  # 删除文件管理文件
 
+x = 0
+y = 0
 for i in JPG_list:
     JPG = open(os.path.join(JPG_file, i), 'rb')
     img = Image.open(JPG)
     width = img.width
     height = img.height
 
-    box = (width * 0.5 - 1493, height * 0.5 - 1528, width * 0.5 + 1493, height * 0.5 + 1528)
-    img = img.crop(box)
-    img.save(os.path.join(JPG_file, i))
+    if width > x:
+        x = width
+
+    if height > y:
+        y = height
+
+print(x)
+print(y)
---
+++ ResizeImage.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os.path
+import glob
+
+
+def convertjpg(jpgfile, outdir, width=640, height=640):
+    img = Image.open(jpgfile)
+    try:
+        new_img = img.resize((width, height), Image.BILINEAR)
+        new_img.save(os.path.join(outdir, os.path.basename(jpgfile)))
+    except Exception as e:
+        print(e)
+
+
+for jpgfile in glob.glob("/Users/lvqianyu/Downloads/untitled folder/DR894393.jpg"):
+    convertjpg(jpgfile, "/Users/lvqianyu/Downloads/untitled folder 2")
