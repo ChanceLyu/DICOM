5409e9c905e51e9781af8a7299a4087c0283a864
chance's mac mini <lqymaster@gmail.com>
2022-01-09 21:23:50 +0800

    update

--- Function/demo.py
+++ Function/demo.py
@@ -1,18 +1,22 @@
 from PIL import Image
 import os
 
-#  按照指定像素裁剪图片
-JPG_file = input('The path of the JPG files: ')  # 图像文件目录
-JPG_list = os.listdir(JPG_file)
-if os.path.exists(JPG_file + '/.DS_Store'):
-    JPG_list.remove('.DS_Store')  # 删除文件管理文件
+for a in ['/Users/lvqianyu/Downloads/JPG分类（1000*1000）/膝关节 阴性',
+          '/Users/lvqianyu/Downloads/JPG分类（1000*1000）/膝关节 阳性',
+          '/Users/lvqianyu/Downloads/JPG分类（1000*1000）/髋关节 阴性',
+          '/Users/lvqianyu/Downloads/JPG分类（1000*1000）/髋关节 阳性']:
+    JPG_file = a  # 图像文件目录
+    JPG_list = os.listdir(JPG_file)
+    if os.path.exists(JPG_file + '/.DS_Store'):
+        JPG_list.remove('.DS_Store')  # 删除文件管理文件
 
-for i in JPG_list:
-    JPG = open(os.path.join(JPG_file, i), 'rb')
-    img = Image.open(JPG)
-    width = img.width
-    height = img.height
+    for i in JPG_list:  # 截取左半边
+        JPG = open(os.path.join(JPG_file, i), 'rb')
+        img = Image.open(JPG)
+        width = img.width
+        height = img.height
 
-    box = (width * 0.5 - 1493, height * 0.5 - 1528, width * 0.5 + 1493, height * 0.5 + 1528)
-    img = img.crop(box)
-    img.save(os.path.join(JPG_file, i))
+        box = (width * 0.5 - 500, height * 0.5 - 500, width * 0.5 + 500, height * 0.5 + 500)
+        img = img.crop(box)
+
+        img.save(JPG_file + '/' + i.strip('.jpg') + '.jpg')
--- ResizeImage.py
+++ ResizeImage.py
@@ -3,7 +3,7 @@ import os.path
 import glob
 
 
-def convertjpg(jpgfile, outdir, width=640, height=640):
+def convertjpg(jpgfile, outdir, width=600, height=600):
     img = Image.open(jpgfile)
     try:
         new_img = img.resize((width, height), Image.BILINEAR)
@@ -12,5 +12,5 @@ def convertjpg(jpgfile, outdir, width=640, height=640):
         print(e)
 
 
-for jpgfile in glob.glob("/Users/lvqianyu/Downloads/untitled folder/DR894393.jpg"):
-    convertjpg(jpgfile, "/Users/lvqianyu/Downloads/untitled folder 2")
+for jpgfile in glob.glob("/Users/lvqianyu/Downloads/JPG分类（1300*1300）/Positive_Knee_DR1027019_1.jpg"):
+    convertjpg(jpgfile, "/Users/lvqianyu/Downloads/JPG分类（1300*1300）")
